6176e34c7d5f26a33a1d3eda1489f4f56ffe0a35
AshwinJadoe <34601644+AshwinJadoe@users.noreply.github.com>
2020-05-25 16:34:50 +0200

    create enigma.py
    + eerste code (al geschreven  in repl)

---
+++ enigma.py
@@ -0,0 +1,12 @@
+import tkinter
+# variabelen-----------
+rotor = ('I', 'II', 'III', 'IV', 'V')
+ringSetPut = np.array(input('ringsettings, 3 letters:\n'), dtype=np.str)
+ringSet = np.char.join(" ", ringSetPut)
+
+#ringpos = input(),input(),input()
+#plugboard = int(input('hoeveel plugboard dingen wil je: \n'))
+#git moet wel bijgehouden worden
+
+print(ringSet)
+
